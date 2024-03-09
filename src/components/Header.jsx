8c732dd6ed2345ecc9dcf4cfcf9c0da0fcdb2a47
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-03-09 20:38:16 +0530

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
@@ -43,21 +43,21 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          }hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
-              <a 
-                key={item.id} 
+              <a
+                key={item.id}
                 href={item.url}
-                onClick = {handleClick}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                    item.onlyMobile ? 'lg:hidden' : ""
-                  } px-6 py-6 md:py-8 lg:mr-0.25 lg:text-xs lg:font-semibold ${
-                    item.url === pathname.hash 
-                      ? 'z-2 lg:text-n-1' 
-                      : 'lg:text-n-1/50'
-                  } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
               </a>
@@ -67,14 +67,14 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-        <a 
-          href="#signup" 
-          className="hidden button mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
+        <a
+          href="#signup"
+          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          New Account
+          New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Sign In
+          Sign in
         </Button>
 
         <Button
@@ -85,9 +85,8 @@ const Header = () => {
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
-      Header
     </div>
-  )
+  );
 };
 
-export default Header
+export default Header;
