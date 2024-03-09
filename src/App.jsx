c8a0556ee3a87813b1bf365afc1963452ea17e0a
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-03-09 12:33:06 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 const App = () => {
   return (
     <>
-      <h1 className='flex justify-center'>Hello World</h1>
       <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
         <Header />
       </div>
