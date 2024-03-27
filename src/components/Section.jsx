9b90fbeb3cbfdb1c3fda5ca707a571accb551314
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-03-27 21:53:52 +0530

    Benefits and Heading added

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,8 +11,11 @@ const Section = ({
   return (
     <div 
         id={id}
-        className={`relative ${
-            customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40' : ''}`
+        className={`
+            relative 
+            ${
+                customPaddings || 
+                `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
         }
         ${
             className || ""
@@ -20,14 +23,15 @@ const Section = ({
     >
         {children}
 
-        <div className='hidden absolute top-0 left-5 w-0.25 f-full bg-stoke-1 pointer-events-none md:block lg:left-7.5 xl:left-10' />
-        <div className='hidden absolute top-0 right-5 w-0.25 f-full bg-stoke-1 pointer-events-none md:block lg:right-7.5 xl:right-10' />
+        <div className='hidden absolute top-0 left-5 w-0.25 h-full bg-stoke-1 pointer-events-none md:block lg:left-7.5 xl:left-10' />
+        <div className='hidden absolute top-0 right-5 w-0.25 h-full bg-stoke-1 pointer-events-none md:block lg:right-7.5 xl:right-10' />
+
         {crosses && (
             <>
                 <div 
                     className={`hidden absolute top-0 left-7.5 right-7.5 h-0.25 bg-stroke-1 ${
                         crossesOffset && crossesOffset
-                    } pointer-events-none lg:block xl: left-10 right-10`}
+                    } pointer-events-none lg:block xl:left-10 right-10`}
                 />
                 <SectionSvg crossesOffset={crossesOffset}/>
             </>
