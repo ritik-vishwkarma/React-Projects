f32e8591b3417ed0fd1f043600ab8ffa357625cc
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-04-13 21:02:50 +0530

    Collaboration part added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Benefits from './components/Benefits';
-import Button from './components/Button';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Collaboration from './components/Collaboration'
 
 const App = () => {
   return (
@@ -11,6 +11,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
       <ButtonGradient />
     </>
