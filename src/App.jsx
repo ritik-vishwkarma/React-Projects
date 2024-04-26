964ae73e06c99bf91e8a655932546f30c13dde12
ritik-vishwkarma <vishwakarmaritik1563@gamil.com>
2024-04-26 13:49:47 +0530

    Add Services and Pricing components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Benefits from './components/Benefits';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Collaboration from './components/Collaboration'
+import Services from './components/Services';
+import Pricing from './components/Pricing';
 
 const App = () => {
   return (
@@ -12,7 +14,10 @@ const App = () => {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
       </div>
+
       <ButtonGradient />
     </>
   )
